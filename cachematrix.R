1bff594f51d631f767f8d0ffdb8db64985726e81
Renan R Praxedes <renan.praxedes@gmail.com>
2016-07-30 19:44:01 -0300

    Assigment task

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Cousera Assigment
 
-## Write a short comment describing this function
+
+
+## create a matrix tov for inverse
+
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  
+  matrixInver = NULL
+  setVal <- function(y){
+    x <<- y
+    matrixInver <<- NULL
+  }
+  
+  
+  getVal <- function() x
+  
+  setInver <- function(inv) matrixInver <<- inv
+  getInver <- function() matrixInver
+  
+  list(setVal = setVal, getVa = getVAL, setInver=setInver, getInver = getInver)
 }
 
 
-## Write a short comment describing this function
+
+
+## Function to work on special matrix
 
 cacheSolve <- function(x, ...) {
+  
         ## Return a matrix that is the inverse of 'x'
+    
+    matrixInv <- x$getInver()
+    ## Already calculate ? 
+    if(!is.null(matrixInv)){
+      message("Already calculated")
+      return (matrixInv)
+      
+    }
+    
+    ## else, set dat
+    mat.data = x$getVal()
+    inv = solve(mat.data, ...)
+    
+    x$setInver(matrixInv)
+    
+    return(matrixInv)
+      
+
 }
+ 
